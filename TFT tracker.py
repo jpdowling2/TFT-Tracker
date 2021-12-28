0a37af80d0debe254f878f13a91c4f9745226a30
jpdowling2 <96754858+jpdowling2@users.noreply.github.com>
2021-12-28 06:47:18 -0600

    Update TFT tracker.py

--- TFT tracker.py
+++ TFT tracker.py
@@ -4,21 +4,36 @@ faced = []
 for i in range(7):
 	opp = input('enter opponents sequentially')
 	possibleOpponents.append(opp)
-    
+
 while True:
-    matchup = input("Enter the most recent opponent:")
-    if len(possibleOpponents) > 3:
-        if matchup in possibleOpponents:
-            possibleOpponents.remove(matchup)
-            faced.append(matchup)
-            print('Possible Opponents are:' + str(possibleOpponents))
-        else:
-            print('Player not found, enter the username correctly')
+    matchup = input('Enter the most recent opponent (type elim to remove a player):')
+    if matchup == 'elim':
+        eliminatedPlayer = input('Enter the name of the eliminated player:')
+        if eliminatedPlayer in possibleOpponents:
+            possibleOpponents.remove(eliminatedPlayer)
+        if eliminatedPlayer in faced:
+            faced.remove(eliminatedPlayer)
+
+    elif matchup == 'stop':
+        break
+
+    elif matchup not in possibleOpponents or faced:
+        print('Player not found.')
+
     else:
-        if matchup in possibleOpponents:
-            possibleOpponents.remove(matchup)
-            faced.append(matchup)
-            possibleOpponents.append(faced[0])
-            print('Possible Opponents are:' + str(possibleOpponents))
+        if len(possibleOpponents) > 3:
+            if matchup in possibleOpponents:
+                possibleOpponents.remove(matchup)
+                faced.append(matchup)
+                print('Possible Opponents are:' + str(possibleOpponents))
+            else:
+                print('Player not found, enter the username correctly')
         else:
-            print('Player not found, enter the username correctly')
+            if matchup in possibleOpponents:
+                possibleOpponents.remove(matchup)
+                faced.append(matchup)
+                possibleOpponents.append(faced[0])
+                faced.remove(faced[0])
+                print('Possible Opponents are:' + str(possibleOpponents))
+            else:
+                print('Player not found, enter the username correctly')
